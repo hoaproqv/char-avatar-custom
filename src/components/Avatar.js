6d9d2c40264e1a249143b32b7b541e5a7ff90930
Han Hoa <hanhoa1997@gmail.com>
2023-06-17 18:07:19 +0700

    update

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Avatar({ listAvatar, handleRandomAvatar }) {
   const list = Object.values(listAvatar);
-  const result = [];
-  for (let i = 0; i < list.length; i++) {
-    result.push(
-      <img key={i} src={list[i]} alt="" height="250px" width="250px" />,
-    );
-  }
+  // const result = [];
+  // for (let i = 0; i < list.length; i++) {
+  //   result.push(
+  //     <img key={i} src={list[i]} alt="" height="250px" width="250px" />,
+  //   );
+  // }
 
   return (
     <>
       <div id="avatar">
-        <div className="image">{result}</div>
+        <div className="image">{list.map((item)=>(<img src={item} alt="" height="250px" width="250px" />))}</div>
         <button id="random" onClick={handleRandomAvatar}>RANDOMIZE!</button>
       </div>
     </>
