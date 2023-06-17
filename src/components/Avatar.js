c6918456c6f2548eef055d24e2487957acd8142e
Han Hoa <hanhoa1997@gmail.com>
2023-06-17 18:07:45 +0700

    update

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,12 +2,6 @@ import React from "react";
 
 function Avatar({ listAvatar, handleRandomAvatar }) {
   const list = Object.values(listAvatar);
-  // const result = [];
-  // for (let i = 0; i < list.length; i++) {
-  //   result.push(
-  //     <img key={i} src={list[i]} alt="" height="250px" width="250px" />,
-  //   );
-  // }
 
   return (
     <>
